8538381b87a40760267215689211d93d8da5852d
woomincheol <wmc1205@naver.com>
2022-05-22 15:50:36 +0900

    2438,2439,2440,10818 완.

--- gradle/wrapper/Main.java
+++ gradle/wrapper/Main.java
@@ -1,23 +1,24 @@
 package gradle.wrapper;
 
 
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int x,y =0;
-         x =scan.nextInt();
-         y = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a =Integer.parseInt(br.readLine());
+        for(int i= 0; i< a;i++){
+            st= new StringTokenizer(br.readLine());
+            bw.write(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+"\n");
+            //개행을 넣어줘야 1줄이 입력되었음을 bw가 파악한다.
 
-        int[] day = {31,28,31,30,31,30,31,31,30,31,30,31};
-        String[] arr = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
-
-        for(int i = 0; i < x -1 ; i++){
-            y += day[i];
         }
-        y = y % 7;
-        System.out.println(arr[y]);
-        scan.close();
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
---
+++ gradle/wrapper/backjoon/BOJ/Main_2445.java
@@ -0,0 +1,44 @@
+package gradle.wrapper.backjoon.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//백준 2445 -> 별찍기 8
+
+
+public class Main_2445 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        br.close();
+        //윗 상단 별찍기 (중간 기준열 포함)
+        for (int i = 1; i <= length; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 2 * length - i; j > 0; j--) {// 공백은 아래로 내려갈 수록 2*length-1개씩 감소한다.
+                if (j > i) { //공백을 찍다가 별이 찍히는 지점
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 2; i <= length; i++) { //i가 2부터 시작되는 이유는 중간줄에 대해 중복을 제거하기 위함이다.
+            for (int j = i; j <= length; j++) { //i가 2부터 시작하기 때문에, (길이-2)+1개의 별이 찍힌다.
+                System.out.print("*");
+            }
+            for (int j = 1; j < 2 * i - 1; j++) {//공백은 아래로 내려갈 수 록 (2*i-1)개씩 증가한다.
+                System.out.print(" ");
+            }
+            for (int j = i; j <= length; j++) {//별이 찍히는 갯수는 좌우 동일하기 때문에 처음 별찍은 로직과 동일하다.
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
