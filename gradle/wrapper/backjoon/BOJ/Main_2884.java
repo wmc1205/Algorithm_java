44d3dcfb7f3446c16474a58df494ad304f548798
woomincheol <wmc1205@naver.com>
2022-06-14 23:41:00 +0900

    2884 완

---
+++ gradle/wrapper/backjoon/BOJ/Main_2884.java
@@ -0,0 +1,23 @@
+package gradle.wrapper.backjoon.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()+ " ");
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        if(H>=0 && H<=23){
+            if(M>=0 && M<=59){
+                if(M<45 && H > 0)   System.out.println(H - 1 + " " + (60 + M - 45));
+                else if(M>=45)      System.out.println(H +" "+(M-45));
+                else                System.out.println((24+H-1)+" " + (60 + M -45));
+            }
+        }
+
+    }
+}
